f0882ed7a98aa82419b5f96b9480fd71a5c9e9e8
jdumezy <jules.dumezy@gmail.com>
2026-08-28 11:51:24 +0200

    Add AVX Rayon backends

--- poulpy-cpu-avx/src/tests/ckks_tests.rs
+++ poulpy-cpu-avx/src/tests/ckks_tests.rs
@@ -11,6 +11,26 @@ ckks_backend_test_suite!(
     rotations = super::ATK_ROTATIONS,
 );
 
+#[cfg(feature = "enable-rayon")]
+ckks_backend_test_suite!(
+    mod fft64_rayon_f64,
+    backend = crate::FFT64AvxRayon,
+    scalar = f64,
+    encoder = crate::FFT64AvxReimTable,
+    params = poulpy_ckks::test_suite::FFT64_PARAMS_F64,
+    rotations = super::ATK_ROTATIONS,
+);
+
+#[cfg(feature = "enable-rayon")]
+ckks_backend_test_suite!(
+    mod ntt4x30_rayon_f64,
+    backend = crate::NTT4x30AvxRayon,
+    scalar = f64,
+    encoder = crate::FFT64AvxReimTable,
+    params = poulpy_ckks::test_suite::NTT4X30_PARAMS_F64,
+    rotations = super::ATK_ROTATIONS,
+);
+
 ckks_backend_test_suite!(
     mod ntt4x30_f64,
     backend = crate::NTT4x30Avx,
